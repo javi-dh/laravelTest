53bec48017b771d9d2a3d7e05e2bd502cf78a2ca
japsolo <jh.docente@gmail.com>
2018-11-21 18:50:25 -0300

    Exercise Movies

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,29 @@
 |
 */
 
-Route::get('/', function () {
-	return ['Hola', 'Chau'];
+$this->movies = [
+	['title' => 'Avatar', 'poster' => 'images/avatar.jpg' , 'genre' => 'Ciencia Ficción'],
+	['title' => 'Infinity War', 'poster' => 'images/avengers.jpg' , 'genre' => 'Acción'],
+	['title' => 'DeadPool', 'poster' => 'images/deadpool1.jpg' , 'genre' => 'Acción'],
+	['title' => 'Dragon Ball', 'poster' => 'images/dragonball.jpg' , 'genre' => 'Animé'],
+	['title' => 'Dunkerque', 'poster' => 'images/dunkirk.jpg' , 'genre' => 'Belico'],
+	['title' => 'Emoji', 'poster' => 'images/emoji.jpg' , 'genre' => 'Animada'],
+	['title' => 'Inception', 'poster' => 'images/inception1.jpg' , 'genre' => 'Drama'],
+	['title' => 'Moana', 'poster' => 'images/moana.jpg' , 'genre' => 'Animada'],
+	['title' => 'Rogue One', 'poster' => 'images/rogueone.jpg' , 'genre' => 'Acción'],
+	['title' => 'Titanic','poster' => 'images/titanic.jpg' , 'genre' => 'Drama']
+];
+
+
+Route::get('/test', function () {
+	echo "<link href='/css/app.css' rel='stylesheet'>";
+	echo "<h1>Listado de películas</h1>";
+	echo "<ul>";
+	foreach ($this->movies as $movie) {
+		echo "<li>" . $movie['title'] . ' - ' . $movie['genre'] . '</li>';
+		echo "<img src='/{$movie['poster']}' width='100'>";
+	}
+	echo "</ul>";
 });
 
 Route::get('/movies', function () {
