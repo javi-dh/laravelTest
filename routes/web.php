cf45d19dded7cd121cb6b18a918bcbc6281bc94d
japsolo <jh.docente@gmail.com>
2018-11-21 16:46:21 -0300

    Routes examples

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,26 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+	return ['Hola', 'Chau'];
+});
+
+Route::get('/movies', function () {
+	dd(['Avatar', 'Titanic']);
+});
+
+Route::get('/hello/{name}/{lastname?}', function ($name, $lastname = null) {
+	if ($lastname) {
+		return 'Hello ' . $name . ' ' . $lastname;
+	}
+
+	return 'Hello ' . $name . ' sin apellido.';
+});
+
+Route::get('/movies/titles', function () {
+	return 'Titles de movies';
+});
+
+Route::get('/saludoMama', function () {
+	$alumnos = ['Dai', 'Dani', 'Ale', 'Fede'];
+	dd($alumnos);
 });
